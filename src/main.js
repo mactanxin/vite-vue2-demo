e0703eddab0098b323edb2e41918d8d6b919d1e3
Xin Tan <mactanxin@gmail.com>
2021-03-16 16:49:33 +0800

    chore(main): Replaced Element UI lib with Vueitfy 2.x

--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import vuetify from './plugins/vuetify';
+// import ElementUI from 'element-ui';
 
-import ElementUI from 'element-ui';
-
-Vue.use(ElementUI);
+// Vue.use(ElementUI);
 import axios from 'axios';
 Vue.prototype.$api = axios;
 Vue.config.productionTip = false;
@@ -13,5 +13,6 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App),
 }).$mount('#app');
